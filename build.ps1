640835a16d6d6a6948157d1a39bb1c8df7ed6604
Meeken1998 <meeken1998@gmail.com>
2025-02-15 01:47:01 +0800

    stash

--- build.ps1
+++ build.ps1
@@ -1,10 +1,12 @@
+# build Atas Indicator
 Set-Location .\atas-indicator
 dotnet build
 if (-not (Test-Path ..\dist)) {
   New-Item -ItemType Directory -Path ..\dist
 }
 Copy-Item .\bin\Debug\net8.0\DgLabAtas.dll ..\dist\DgLabAtasIndicator.dll
-# 进入 venv，进入 server 目录，并执行 pyinstaller --onefile --icon=icon.ico my_script.py
+
+# build server
 Set-Location ..\server
 python -m venv venv
 .\venv\Scripts\activate
